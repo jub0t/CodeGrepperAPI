830168f978fb966d54b31d9d8a8ea5c990918516
JUB0T <jforeverything2007@gmail.com>
2021-08-05 21:50:25 +0200

    Create cookieLogger.js

---
+++ cookieLogger.js
@@ -0,0 +1,2 @@
+console.log(`grepper_web_access_token=${localStorage.access_token}; PHPSESSID=${localStorage.PHPSESSID}`)
+console.log(`${document.cookie.split(" ")[0]} ${document.cookie.split(" ")[6]};`)
