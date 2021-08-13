756af977080e9097afaa46e344a9a8e00f1c7fcf
JUB0T <jforeverything2007@gmail.com>
2021-08-13 22:32:57 +0200

    new version i guess

--- Code Grepper UI/theme.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-} else {
-    document.documentElement.classList.remove('dark')
-}
-function darktheme() { localStorage.removeItem('theme'); localStorage.theme = 'dark'; console.log(localStorage.theme); location.reload() }
-function lighttheme() { localStorage.theme = 'light'; localStorage.theme = 'light'; console.log(localStorage.theme); location.reload() }
