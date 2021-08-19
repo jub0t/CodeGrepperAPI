a35ba4f748c35e87e7c99f4daa765653a6a175a9
JUB0T <jforeverything2007@gmail.com>
2021-08-19 21:41:08 +0200

    idk...

--- Bots/follow.py
+++ Bots/follow.py
@@ -4,7 +4,6 @@ import json
 import secrets
 import threading
 from colorama import init
-from termcolor import colored
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
