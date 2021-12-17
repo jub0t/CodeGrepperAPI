c9a5d13c5f589df7eb918f34416ae4eda42d6f43
JUB0T <jforeverything2007@gmail.com>
2021-12-17 01:53:48 +0100

    update
    pogg

--- Bots/follow.py
+++ Bots/follow.py
@@ -41,30 +41,30 @@ print('\033[34m', "Target Id:" + '\033[32m', user_Id,
       '\033[34m', "Estimated Time To Deliver All" + '\033[32m', max_followers, "Follows:", '\033[32m' + str(round(max_followers * 9 / 60)) + " Minutes")
 
 for i in range(0, max_followers):
-   random_string = secrets.token_hex(nbytes=16)
-   chrome_driver_path = "./assets/Driver.exe"
+    random_string = secrets.token_hex(nbytes=16)
+    chrome_driver_path = "./assets/Driver.exe"
 
-   driver = webdriver.Chrome(chrome_driver_path, options=Options)
-   driver.get("https://www.codegrepper.com/index.php")
+    driver = webdriver.Chrome(chrome_driver_path, options=Options)
+    driver.get("https://www.codegrepper.com/index.php")
 
-   signup = driver.find_element_by_xpath("//*[text()='Signup']")
-   signup.click()
-   time.sleep(2)
+    signup = driver.find_element_by_xpath("//*[text()='Signup']")
+    signup.click()
+    time.sleep(2)
 
-   email_input = driver.find_element_by_class_name("input1")
-   email_input.send_keys(random_string, "@jub0t.com")
+    email_input = driver.find_element_by_class_name("input1")
+    email_input.send_keys(random_string, "@jub0t.com")
 
-   pass_input = driver.find_element_by_name("password")
-   pass_input.send_keys("jub0t123")
+    pass_input = driver.find_element_by_name("password")
+    pass_input.send_keys("jub0t123")
 
-   register = driver.find_element_by_id("register_button")
-   register.click()
-   time.sleep(2)
+    register = driver.find_element_by_id("register_button")
+    register.click()
+    time.sleep(2)
 
-   driver.get(
-       "https://www.codegrepper.com/api/follow.php?follow_user_id=" + str(user_Id) + "&follow=1")
+    driver.get(
+        "https://www.codegrepper.com/api/follow.php?follow_user_id=" + str(user_Id) + "&follow=1")
 
-   print('\033[34m', 'Follower Attempt', i + 1, ":", '\033[32m', "Success")
-   driver.quit()
+    print('\033[34m', 'Follower Attempt', i + 1, ":", '\033[32m', "Success")
+    driver.quit()
 
 print('\033[34m', '<▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ Task Complete  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬>')
--- Scripts/deleteNagitiveAnswers.js
+++ Scripts/deleteNagitiveAnswers.js
@@ -1,24 +1,61 @@
 /* 
+    Since The Grepper Contributor Coin System is Poping out, I Guess
+    I need to get rid of my negative answers.
+
     Paste in Console, While on Codegrepper.com. (Page Doesnt Matter)
     Preview: https://i.gyazo.com/c042932310fe6a510317107624526026.png
     After: Manually Delete The Answers
+
+    NOTE: Make sure to use a vpn as you might get IP Banned.
 */
 
 const minimumScore = 0; // Show Answers Like & Less Than This.
-const queryPulls = 5; // Logs Around 5(5 * 10) Queries, 5 means fetches 5 times.
-for (i = 0; i < queryPulls; i++) {
-    if (i <= queryPulls) {
-        fetch('https://www.codegrepper.com/api/get_all_users_answers2.php?&offset=100&sort_by=score_asc')
-            .then((response) => {
-                return response.json();
-            })
-            .then((Data) => {
-                for (i = 0; i < Data.answers.length; i++) {
-                    const answerScore = parseFloat(Data["answers"][i].score)
-                    if (answerScore <= minimumScore) {
-                        console.log(`[${answerScore}]: https://www.google.com/search?q=${encodeURI(Data["answers"][i].term.toString())}`)
-                    }
-                }
-            });
+function userID() {
+    if (localStorage.user_id) {
+        return localStorage.user_id;
     }
 }
+
+setInterval(function () {
+    fetch(`https://www.codegrepper.com/api/get_all_users_answers2.php?sort_by=score_asc`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((Data) => {
+            for (i = 0; i < Data.answers.length; i++) {
+                const answerScore = parseFloat(Data["answers"][i].score)
+                if (answerScore <= minimumScore) {
+                    fetch(`https://www.codegrepper.com/api/get_answers_1.php?v=3&s=${Data["answers"][i].term}&u=${userID()}`)
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((JSON) => {
+                            for (j = 0; j < JSON.answers.length; j++) {
+                                console.log(`${JSON.answers.length - j} Answers Left`)
+                                if (JSON.answers[j]) {
+                                    if (JSON.answers[j].user_id == userID()) {
+                                        fetch(`https://www.codegrepper.com/api/delete.php?id=${JSON.answers[j].id}&u=${userID()}`, {
+                                            method: 'POST',
+                                            headers: { 'Content-Type': 'application/json' },
+                                        })
+                                            .then((res) => res.json())
+                                            .then((data) => {
+                                                console.log(`${JSON.answers[j].id} Answer Deleted`)
+                                            })
+                                            .catch((err) => {
+                                                if (JSON.answers[j]) {
+                                                    console.log(`Was Unable To Delete ${JSON.answers[j].id}`)
+                                                } else {
+                                                    console.log(`Was Unable To Delete`)
+                                                }
+                                            });
+                                    } else {
+                                        console.log(`Answer Fetch But You Are Not The Owner, Ignore`)
+                                    }
+                                }
+                            }
+                        });
+                }
+            }
+        });
+}, 3000)
