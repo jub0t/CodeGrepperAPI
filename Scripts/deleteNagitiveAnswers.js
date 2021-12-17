70e46fb3a4b4dcb06d4dedbbf594a4967db7acd2
JUB0T <jforeverything2007@gmail.com>
2021-12-17 02:15:54 +0100

    Update deleteNagitiveAnswers.js

--- Scripts/deleteNagitiveAnswers.js
+++ Scripts/deleteNagitiveAnswers.js
@@ -9,14 +9,14 @@
     NOTE: Make sure to use a vpn as you might get IP Banned.
 */
 
-const minimumScore = 0; // Show Answers Like & Less Than This.
-function userID() {
-    if (localStorage.user_id) {
-        return localStorage.user_id;
-    }
-}
-
 setInterval(function () {
+    const minimumScore = 0; // Show Answers Like & Less Than This.
+    function userID() {
+        if (localStorage.user_id) {
+            return localStorage.user_id;
+        }
+    }
+
     fetch(`https://www.codegrepper.com/api/get_all_users_answers2.php?sort_by=score_asc`)
         .then((response) => {
             return response.json();
@@ -25,22 +25,23 @@ setInterval(function () {
             for (i = 0; i < Data.answers.length; i++) {
                 const answerScore = parseFloat(Data["answers"][i].score)
                 if (answerScore <= minimumScore) {
-                    fetch(`https://www.codegrepper.com/api/get_answers_1.php?v=3&s=${Data["answers"][i].term}&u=${userID()}`)
+                    fetch(`https://www.codegrepper.com/api/get_answers_1.php?v=3&s=${encodeURI(Data["answers"][i].term)}&u=${userID()}`)
                         .then((response) => {
                             return response.json();
                         })
                         .then((JSON) => {
                             for (j = 0; j < JSON.answers.length; j++) {
-                                console.log(`${JSON.answers.length - j} Answers Left`)
+                                console.log(JSON)
                                 if (JSON.answers[j]) {
+                                    console.log(`${JSON.answers[j].user_id} ${userID()}`)
                                     if (JSON.answers[j].user_id == userID()) {
-                                        fetch(`https://www.codegrepper.com/api/delete.php?id=${JSON.answers[j].id}&u=${userID()}`, {
+                                        fetch(`https://www.codegrepper.com/api/delete.php?id=${encodeURI(JSON.answers[j].id)}&u=${userID()}`, {
                                             method: 'POST',
                                             headers: { 'Content-Type': 'application/json' },
                                         })
                                             .then((res) => res.json())
                                             .then((data) => {
-                                                console.log(`${JSON.answers[j].id} Answer Deleted`)
+                                                console.log(`${JSON.answers[j].id} Answer Deleted, Had ${JSON.answers[j].score} Score`)
                                             })
                                             .catch((err) => {
                                                 if (JSON.answers[j]) {
@@ -50,7 +51,7 @@ setInterval(function () {
                                                 }
                                             });
                                     } else {
-                                        console.log(`Answer Fetch But You Are Not The Owner, Ignore`)
+                                        console.log(`Answer Fetched But You Are Not The Owner, Ignoring`)
                                     }
                                 }
                             }
